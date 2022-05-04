30c3fdff6404b5eb8888dd69d70a3cd207772418
aluno <aluno@DSN1251017043>
2022-05-04 08:59:06 -0300

    organização do commit

--- ProjetoHorário/src/Data.java
+++ ProjetoHorário/src/Data.java
@@ -10,58 +10,57 @@ public class Data {
 	private int hora = -1;
 	private int minuto = -1;
 	private int segundo = -1;
-	
+
 	public Data(int dia, int mes, int ano) {
 		this.dia = dia;
 		this.mes = mes;
 		this.ano = ano;
 	}
+
 	public Data(int dia, int mes, int ano, int hora, int minuto, int segundo) {
-		this (dia, mes, ano);
+		this(dia, mes, ano);
 		this.hora = hora;
 		this.minuto = minuto;
 		this.segundo = segundo;
 	}
-	
+
 	public void imprimir(int formato) {
 		String data = dia + "/" + mes + "/" + ano;
-		
+
 		if (hora == -1) {
 			System.out.println(data);
-		}
-		else {
+		} else {
 			String horario = ":" + minuto + ":" + segundo;
-		
-	
-	if (formato == FORMATO_24H) {
-		horario = hora + horario;
-	}else {
-		if (hora == 0) {
-			horario = 12 + horario;
-			horario += " AM";
+
+			if (formato == FORMATO_24H) {
+				horario = hora + horario;
+			} else {
+				if (hora == 0) {
+					horario = 12 + horario;
+					horario += " AM";
+				} else if (hora >= 12) {
+					horario = (hora - 12) + horario;
+					horario += " PM";
+				} else {
+					horario = hora + horario;
+					horario += " AM";
+				}
+
+			}
+
+			System.out.println(data + " " + horario);
 		}
-		else if (hora >= 12) {
-			horario = (hora - 12) + horario;
-			horario += " PM";
-		}else {
-			horario = hora + horario;
-			horario += " AM";
-		}
-		
+
 	}
-		
-	System.out.println(data + " " + horario);
-		}
-		
-	}
+
 	public int getDia() {
 		return dia;
 	}
-	
+
 	public int getMes() {
 		return mes;
 	}
-	
+
 	public int getAno() {
 		return ano;
 	}
@@ -78,8 +77,4 @@ public class Data {
 		return segundo;
 	}
 
-	}
-	
-
-	
-	
+}
