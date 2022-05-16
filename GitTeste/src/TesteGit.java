3c67521b82dee97c82fa8ef82157e8c0ac8eff0b
Marcos V. Oliveira <102962949+ViniMarkos283@users.noreply.github.com>
2022-05-16 08:38:06 -0300

    Update TesteGit.java

--- GitTeste/src/TesteGit.java
+++ GitTeste/src/TesteGit.java
@@ -4,8 +4,8 @@ public class TesteGit {
 	public static void main(String[] args) {
 		
 		System.out.println("hello World!");
-	//arquivo de teste, usado  só pra testar o github"
-		
+	// arquivo de teste, usado  sÃ³ pra testar o envio de arquivo pelo github
+	// teste do comentario via github.	
 	}
 
 }
