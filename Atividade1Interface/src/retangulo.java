7906eff8d792692b05f8afbebf4adf0d289b443f
Marcos Vinicius de Oliveira <102962949+ViniMarkos283@users.noreply.github.com>
2022-05-26 11:12:59 -0300

    Update retangulo.java

--- Atividade1Interface/src/retangulo.java
+++ Atividade1Interface/src/retangulo.java
@@ -2,8 +2,8 @@
 public class retangulo implements AreaCalculavel{
 
 	@Override
-	public void calcularArea(double x, double y) {
-		System.out.println("A area do seu retangulo é: " + x * y);
+	public void calcularArea(double x, double y) { // formula do retangulo
+		System.out.println("A area do seu retangulo Ã©: " + x * y);
 	}
 
 
