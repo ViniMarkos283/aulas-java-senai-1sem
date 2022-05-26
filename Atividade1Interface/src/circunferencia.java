22f1dfe5d6fb948ab94483d18dc1950bd5a123f0
Marcos Vinicius de Oliveira <102962949+ViniMarkos283@users.noreply.github.com>
2022-05-26 11:12:30 -0300

    Update circunferencia.java

--- Atividade1Interface/src/circunferencia.java
+++ Atividade1Interface/src/circunferencia.java
@@ -2,8 +2,8 @@
 public class circunferencia implements AreaCalculavel{
 
 	@Override
-	public void calcularArea(double x, double y) {
-		System.out.println("A area da sua circunferencia é: " + 2 * Math.PI * x);		
+	public void calcularArea(double x, double y) { // formula da circunferencia, que vai sobescrever o metodo calcular area
+		System.out.println("A area da sua circunferencia Ã©: " + 2 * Math.PI * x);		
 	}
 
 }
